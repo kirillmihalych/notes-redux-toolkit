27b8c69f419ec6c302474e81dd9305386139b6e6
KIRILL F <tiltlessman@gmail.com>
2022-08-23 10:36:35 +0300

    [8] Create Navbar, add link to the Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import SingleNotePage from './features/notes/SingleNotePage'
 import Home from './pages/Home'
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route index element={<Home />} />
         <Route path='/notes/:id' element={<SingleNotePage />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <Wrapper>
+      <section className='nav-content'>
+        <h2 className='nav-title'>Заметки</h2>
+        <div className='nav-links'>
+          <Link to='/' className='nav-link'>
+            Домой
+          </Link>
+        </div>
+      </section>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  background: rgba(238, 238, 238, 0.3);
+  .nav-content {
+    width: 90vw;
+    max-width: 1170px;
+    margin: 0 auto;
+  }
+  .nav-links {
+    display: flex;
+  }
+  .nav-link {
+    text-decoration: none;
+    font-size: 1.25rem;
+    text-transform: capitalize;
+    padding: 10px;
+    background: var(--black);
+    color: var(--white);
+  }
+  .nav-link:hover {
+    color: var(--gold);
+  }
+`
+
+export default Navbar
